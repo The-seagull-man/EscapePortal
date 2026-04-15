180fb480632011ac270699faab4245116989b166
Brotospingvin <153622518+Brotospingvin@users.noreply.github.com>
2026-04-15 10:10:30 +0200

    activator schanges

--- My project/Assets/Scripts/ActivatorBattery.cs
+++ My project/Assets/Scripts/ActivatorBattery.cs
@@ -2,12 +2,18 @@ using UnityEngine;
 
 public class ActivatorBattery : Activator
 {
+	public float size;
     void OnCollisionEnter(Collision collision) {
 		if (!Active) {
 			//If the object that collides is a battery, consume the battery to turn on the machine.
 			if (collision.gameObject.CompareTag("Battery")) {
-				Active = true;
-				Destroy(collision.gameObject);
+				if (collision.gameObject.transform.localScale.x == size)
+				{
+					Active = true;
+					Debug.Log("active");
+					Destroy(collision.gameObject);
+
+				}
 			}
 		}
 	}
