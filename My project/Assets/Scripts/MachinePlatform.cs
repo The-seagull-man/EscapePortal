a6099a47ab04f621f480604e1335b44fcd3b627b
1rasmusrune1 <1rasmusrune1@gmail.com>
2026-04-15 17:48:21 +0200

    Updated moving platform to use physics
    The moving platform now uses the physics system to move, instead of just teleporting along the path every frame. It is a bit jank.

--- My project/Assets/Scripts/MachinePlatform.cs
+++ My project/Assets/Scripts/MachinePlatform.cs
@@ -4,55 +4,48 @@ public class MachinePlatform : Machine {
 	public Transform destination;
 	public float travelTime;
 	public float endpointPause;
+	public float acceleration;
+	public float friction;
+	public float minTimeLeft;
 
 	Vector3 origin;
 	Vector3 dest;
-	float travelFactor;
+	Rigidbody rb;
+	float currentTravelTime;
 	bool reversing;
-	float pauseTime;
 
 	void Start() {
 		origin = transform.position;
 		dest = destination.position;
 		Destroy(destination.gameObject); //We don't need to keep the destination object, now that we have saved the position.
+		rb = GetComponent<Rigidbody>();
 	}
 
 	// Update is called once per frame
 	void FixedUpdate()
     {
         if (Active) {
-			//If the platform is not paused.
-			if (pauseTime == 0) {
-				//Calculate amount to move per frame.
-				float travelDelta = Time.fixedDeltaTime/travelTime;
-				if (reversing) {
-					travelFactor -= travelDelta;
-					if (travelFactor <= 0) {
-						reversing = false;
-						travelFactor = 0;
-						pauseTime = endpointPause;
-					}
-				} else {
-					travelFactor += travelDelta;
-					if (travelFactor >= 1) {
-						reversing = true;
-						travelFactor = 1;
-						pauseTime = endpointPause;
-					}
-				}
-				transform.position = Vector3.Lerp(origin, dest, travelFactor);
-			} else {
-				//Tick down pause timer.
-				pauseTime -= Time.fixedDeltaTime;
-				if (pauseTime < 0) {
-					pauseTime = 0;
-				}
+			Vector3 direction = (reversing ? origin : dest) - transform.position;
+			float distance = direction.magnitude;
+			direction = direction.normalized;
+
+			float speed = rb.linearVelocity.magnitude;
+			if (speed != 0) {
+				speed = speed*Mathf.Max(Vector3.Dot(rb.linearVelocity/speed, direction), 0);
+			}
+			float timeLeft = travelTime - currentTravelTime;
+			rb.AddForce(direction*(distance/Mathf.Max(timeLeft, minTimeLeft) - speed)*acceleration - rb.linearVelocity*friction, ForceMode.Acceleration);
+
+			currentTravelTime += Time.fixedDeltaTime;
+			if (currentTravelTime >= travelTime + endpointPause) {
+				currentTravelTime -= travelTime + endpointPause;
+				reversing = !reversing;
 			}
 		}
     }
 
 	public override void OnActivate() {
-		pauseTime = endpointPause;
+		rb.isKinematic = false;
 	}
 
 	public override void OnDeactivate() {}
