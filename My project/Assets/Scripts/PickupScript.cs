3db4415da57d8034c32f3a09f262d8eade4a0810
1rasmusrune1 <1rasmusrune1@gmail.com>
2026-04-30 08:56:25 +0200

    Picked up objects spin less
    Previously, picked up objects would often start spinning, after which they wouldn't stop.
    
    Now, when you pick up an object, its angular damping will be set to the hardcoded constant .5, which will cause it to stop spinning. Its angular damping gets reset when it's dropped.

--- My project/Assets/Scripts/PickupScript.cs
+++ My project/Assets/Scripts/PickupScript.cs
@@ -7,23 +7,26 @@ public class PickupScript : MonoBehaviour
 	public float maxDistance = 5;
 
 	Rigidbody rb;
+	float angularDamping;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+    void Start() {
+		rb = GetComponent<Rigidbody>();
+		angularDamping = rb.angularDamping;
+	}
 
-    public virtual void OnPickup() {
+	public virtual void OnPickup() {
         rb.useGravity = false;
 		rb.linearVelocity = Vector3.zero;
 		rb.angularVelocity = Vector3.zero;
+		rb.angularDamping = .5f;
 	}
 
     public virtual void OnDrop(Vector3 direction, bool thrown = true) {
 		rb.useGravity = true;
 		rb.linearVelocity = Vector3.zero;
 		rb.angularVelocity = Vector3.zero;
+		rb.angularDamping = angularDamping;
 		if (thrown) {
 			rb.AddForce(direction*throwForce, ForceMode.Impulse);
 		}
