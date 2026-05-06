e1851682e3e75c41cec244603a5a0be533e7e8d4
1rasmusrune1 <1rasmusrune1@gmail.com>
2026-05-06 13:51:04 +0200

    Changed shatter system
    A shatterable object now uses at least two scripts: One script which handles the shattering (Shatterable), and a script which triggers the shattering (like ShatterForce and ShatterBrick). This way, an object can have multiple different ways of shattering without having to repeatedly define the shatter properties.
    
    Also fixed some leftover scripts on glass cube and explosive from the old object respawner system.

--- My project/Assets/Scripts/ShatterBrick.cs
+++ My project/Assets/Scripts/ShatterBrick.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
 
-public class ShatterBrick : Shatterable {
+public class ShatterBrick : MonoBehaviour {
+	public Shatterable shatter;
 	public float shatterForce = 7;
 	public string brickTag;
 
 	public void OnCollisionEnter(Collision collision) {
 		if (collision.gameObject.CompareTag(brickTag) && collision.relativeVelocity.magnitude > shatterForce) {
-			Shatter(CalculateReflectVector(collision));
+			shatter.Shatter(shatter.CalculateReflectVector(collision));
 		}
 	}
 }
--- My project/Assets/Scripts/ShatterForce.cs
+++ My project/Assets/Scripts/ShatterForce.cs
@@ -1,17 +1,18 @@
 using UnityEngine;
 
-public class ShatterForce : Shatterable, ExplosionReceiver {
+public class ShatterForce : MonoBehaviour, ExplosionReceiver {
+	public Shatterable shatter;
 	public float shatterForce = 6.2f;
 
 	public void OnCollisionEnter(Collision collision) {
 		if (collision.relativeVelocity.magnitude > shatterForce) {
-			Shatter(CalculateReflectVector(collision));
+			shatter.Shatter(shatter.CalculateReflectVector(collision));
 		}
 	}
 
 	public void ReceiveExplosion(Vector3 position, Vector3 force, float power) {
 		if (power > shatterForce) {
-			Shatter(force.normalized);
+			shatter.Shatter(force.normalized);
 		}
 	}
 }
