89c72a141aac419a7e58edf8f033d0ed046c1500
Shunthenerd <Suneegholmlyngbak@gmail.com>
2026-05-18 09:09:01 +0200

    joih

--- My project/Assets/Scripts/Portal Scripts/PortalCounting.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-public class PortalCounting // this script is not beign used
-{
-    public GameObject GameObject;
-    public int PortalWarpCount;
-    public bool CanWarp;
-}
